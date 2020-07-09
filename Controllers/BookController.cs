1b0d226ac370b1960fcfc2ca6574abb30a420123
austin <ap.hugus@gmail.com>
2020-07-09 15:38:32 -0600

    books works

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -5,73 +5,100 @@ using libraryapi.Models;
 using libraryapi.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace libraryapi.Controllers {
+namespace libraryapi.Controllers
+{
 
     [ApiController]
-    [Route ("api/[controller]")]
+    [Route("api/[controller]")]
 
-    public class BookController : ControllerBase {
+    public class BookController : ControllerBase
+    {
 
         private readonly BooksService _service;
 
-        public BookController (BooksService service) {
+        public BookController(BooksService service)
+        {
             _service = service;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Book>> GetAll () {
-            try {
-                return Ok (_service.Get ());
+        public ActionResult<IEnumerable<Book>> GetAll()
+        {
+            try
+            {
+                return Ok(_service.Get());
 
-            } catch (Exception e) {
-                return BadRequest (e.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
 
-        [HttpGet ("available")]
-        public ActionResult<IEnumerable<Book>> GetAvail () {
-            try {
-                return Ok (_service.Get ());
-            } catch (Exception e) {
-                return BadRequest (e.Message);
+        [HttpGet("available")]
+        public ActionResult<IEnumerable<Book>> GetAvail()
+        {
+            try
+            {
+                return Ok(_service.Get());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
 
-        [HttpGet ("{id}")]
-        public ActionResult<Book> GetById (string id) {
-            try {
-                return Ok (_service.GetById (id));
-            } catch (Exception e) {
-                return BadRequest (e.Message);
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetById(string id)
+        {
+            try
+            {
+                return Ok(_service.GetById(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
 
         [HttpPost]
 
-        public ActionResult<Book> Create ([FromBody] Book newBook) {
-            try {
-                return Ok (_service.Create (newBook));
-            } catch (Exception e) {
-                return BadRequest (e.Message);
+        public ActionResult<Book> Create([FromBody] Book newBook)
+        {
+            try
+            {
+                return Ok(_service.Create(newBook));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
 
-        [HttpPut ("{id}")]
+        [HttpPut("{id}")]
 
-        public ActionResult<Book> Edit ([FromBody] Book editBook, string id) {
-            try {
-                return Ok (_service.Edit (editBook, id));
-            } catch (Exception e) {
-                return BadRequest (e.Message);
+        public ActionResult<Book> Edit([FromBody] Book editBook, string id)
+        {
+            try
+            {
+                return Ok(_service.Edit(editBook, id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
 
-        [HttpDelete ("{bookId")]
-        public ActionResult<Book> Delete (string bookId) {
-            try {
-                return Ok (_service.Delete (bookId));
-            } catch (Exception e) {
-                return BadRequest (e.Message);
+        [HttpDelete("{bookId}")]
+        public ActionResult<Book> Delete(string bookId)
+        {
+            try
+            {
+                return Ok(_service.Delete(bookId));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
 
